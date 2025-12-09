52c519fbf09cbf33c73fdca52d1b0ad567176d48
TKtkGg <yuki.miyamoto.0729@icloud.com>
2025-12-09 20:53:29 +0900

    サインアップ、ログイン、ログアウト機能① startappとbase.htmlを新しく作成しました。

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/',admin.site.urls),
-    path('', include('game.urls')),
+    path('', include('accounts.urls')),
+    path('game/', include('game.urls')),
 ]
