525ea24b78b569e00239a29b1905b456a26a0de9
TKtkGg <yuki.miyamoto.0729@icloud.com>
2026-03-18 00:09:40 +0900

    ログイン、ログアウトAPIの作成

--- backend/accounts/api_urls.py
+++ backend/accounts/api_urls.py
@@ -3,4 +3,6 @@ from . import api_views
 
 urlpatterns = [
     path('session/', api_views.session_view, name='auth-session'),
+    path('login/', api_views.login_view, name='auth-login'),
+    path('logout/', api_views.logout_view, name='auth-logout'),
 ]
--- backend/accounts/api_views.py
+++ backend/accounts/api_views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.contrib.auth import authenticate, login, logout
 
 def session_view(request):
     is_authenticated = request.user.is_authenticated
@@ -9,4 +10,35 @@ def session_view(request):
             "id": user.id,
             "username": user.username,
         } or None,
+    })
+
+def login_view(request):
+    if request.method != 'POST':
+        return JsonResponse({
+            "error": "Method not allowed"
+        }, status=405)
+    
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return JsonResponse({
+            "ok": True
+        })
+    else:
+        return JsonResponse({
+            "ok": False,
+            "error": "Invalid username or password"
+        }, status=401)
+
+def logout_view(request):
+    if request.method != 'POST':
+        return JsonResponse({
+            "error": "Method not allowed"
+        }, status=405)
+        
+    logout(request)
+    return JsonResponse({
+        "ok": True
     })
